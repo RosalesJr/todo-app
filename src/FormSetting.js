257f34b535669df8b21ef3fa1f88e24502d9a37b
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-11-02 06:17:57 +0000

    implemented browser router

---
+++ src/FormSetting.js
@@ -0,0 +1,11 @@
+const FormSetting = () => {
+  return(
+    <>
+    <div>
+      <p>Hello World</p>
+    </div>
+    </>
+  );
+};
+
+export default FormSetting;
--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 import ToDo from './Components/ToDo';
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer';
+import FormSetting from './FormSetting';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
-        <Header />
-        <ToDo />
-        <Footer />
+        <Router>
+          <Header />
+          <Routes>
+          <Route path="/"
+            element={<ToDo />}>
+          </Route>
+          <Route path="/setting"
+            element={<FormSetting />}>
+          </Route>
+          </Routes>
+          <Footer />
+        </Router>
       </>
     );
   }
